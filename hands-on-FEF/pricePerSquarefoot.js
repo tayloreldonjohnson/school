757f7bf320d2c01f0fc734c42759105d861b9cb6
Unknown <tockstaunt@gmail.com>
2018-01-09 22:47:53 -0600

    moved folders for better long term storage

--- hands-on-FEF/pricePerSquarefoot.js
+++
@@ -1,7 +0,0 @@
-function totalPrice(sqEdge, pricePerSqFt) {	
-	var totalCost = sqEdge   * sqEdge * pricePerSqFt;
-	document.getElementById("totalCost").innerHTML = totalCost;	
-	return totalCost;
-}
-totalPrice(10, 10);
-
--- stretch-goal1/goal1.js
+++
@@ -1,6 +0,0 @@
-const day = 26;
-const month = "December";
-const year = 1986;
-
-const myBirthday = month+" "+day+", "+year;
-console.log(myBirthday);
