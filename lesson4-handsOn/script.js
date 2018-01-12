c01a535c84ecc4b8145c495fbdb3194ac3e9a532
Unknown <tockstaunt@gmail.com>
2018-01-12 02:06:09 -0600

    added lesson 3 - finished
    added finished lesson 3 hands on and add starter files for lesson 4 hands on

---
+++ lesson4-handsOn/script.js
@@ -0,0 +1,9 @@
+/* function phoneNumberFormat(phoneNumber) {
+    phoneNumber = document.getElementById("phNumber").value;
+    let phoneRegex = /^\(\d\d\d\) \d\d\d-\d\d\d\d$/g;
+    if (phoneNumber.match(phoneRegex)) {
+      alert("Valid number entered");
+    } else {
+      alert("Phone number entered is invalid");
+    }
+  } convert code to ternary */
---
+++ lesson4-strecth-goal1/script.js
@@ -0,0 +1,3 @@
+/* Leverage your knowledge of the JavaScript programming language to write a try...catch statement using the given variable; it should have at least three try statements such as if (x < 5) throw true;. Your catch should log the error to the console.
+ */
+let newNumber = 10;
