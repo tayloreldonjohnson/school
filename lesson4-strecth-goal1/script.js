b08fb37e369806dbabbe6405fdf2c269adcc3751
Unknown <tockstaunt@gmail.com>
2018-01-14 00:57:56 -0600

    finished stretch goal 1 lesson4

--- lesson4-strecth-goal1/script.js
+++ lesson4-strecth-goal1/script.js
@@ -1,3 +1,11 @@
-/* Leverage your knowledge of the JavaScript programming language to write a try...catch statement using the given variable; it should have at least three try statements such as if (x < 5) throw true;. Your catch should log the error to the console.
- */
 let newNumber = 10;
+
+try{
+    if(newNumber === 10)throw 1;
+    if(newNumber <= 5)throw 2;
+    if(newNumber === 5)throw 3;
+
+}catch(err){
+    console.log(err);
+    
+}
