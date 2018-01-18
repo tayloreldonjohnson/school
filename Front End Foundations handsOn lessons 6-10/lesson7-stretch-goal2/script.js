942e0b3bf85484fb38f6ade6530abe6527393bba
Unknown <tockstaunt@gmail.com>
2018-01-17 22:54:45 -0600

    finished lesson 7 handsOn stretch goal 2

--- Front End Foundations handsOn lessons 6-10/lesson7-stretch-goal2/script.js
+++ Front End Foundations handsOn lessons 6-10/lesson7-stretch-goal2/script.js
@@ -1,12 +1,30 @@
-/* Leverage your knowledge of AJAX to create a web page that displays a famous historical figure with a header and a button when clicked displays more information about the historical figure.
+//function that GETs info.json and places in <p>s on webpage when clicked
+function einsteinBio(){
+    const einBio = new XMLHttpRequest();
+    einBio.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200 || this.status == 304){
+                let einBioJson = JSON.parse(this.responseText);                
+                document.getElementById("einBioContent").innerHTML = einBioJson.quote1;
+                document.getElementById("einBioContent1").innerHTML = einBioJson.quote2;
+                document.getElementById("einBioContent2").innerHTML = einBioJson.quote3;
 
-For example:
+            }
+    }
+    einBio.open("GET", "info.json", true);
+    einBio.send();
+}
 
-If you chose Einstein, there should be:
-
-    A header with his name,
-    A picture of Einstein,
-    A button to "Learn More" and
-    A paragraph about Einstein that is displayed once the button is clicked.
-
-This will require you to create a separate .txt file to store your information. */
+//not sure if i like the spacing on this css elements changes
+$(document).ready(function(){;
+    $("img").css("margin-left","30%");
+    $("img").css("margin-right","auto");
+    $("img").css("width","25%");
+    $(".para").css("width","50%");
+    $(".para").css("margin","1%");
+    $(".para").css("margin-left","15%");
+    $(".para").css("margin-right","auto");
+    $("button").css("margin-left","30%");
+    $("button").css("margin-right","auto");
+    $(".h1E").css("margin-left","15%");
+    $(".h1E").css("margin-right","auto");
+});
